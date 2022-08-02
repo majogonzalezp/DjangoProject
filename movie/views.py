5b5584cb15e1460394625c74af57ed8c6bda8e98
Maria Jose <mjgonzalep@eafit.edu.co>
2022-08-02 11:01:58 -0500

    Agregue about.html y cambio de color al nombre en home.html

--- movie/views.py
+++ movie/views.py
@@ -6,3 +6,8 @@ def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
     return render(request, 'home.html', {'name': 'María José González Peláez'})
+
+def about(request):
+    #return HttpResponse('<h1>Welcome to Home Page</h1>')
+    #return render(request, 'home.html')
+    return render(request, 'about.html')
